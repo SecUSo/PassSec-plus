e2be101196a2fd5191dc4a71045c70df4763dcdf
Johannes <wagener.johannes@googlemail.com>
2017-09-26 20:12:17 +0200

    optimized tooltip to match the behaviour of the XUL version

--- js/contentscript.js
+++ js/contentscript.js
@@ -5,66 +5,61 @@ passSec.api;
 passSec.tooltip;
 passSec.target;
 
-jQuery(function($){
-  chrome.runtime.sendMessage({"name":'TLD'}, function(tld){
-    passSec.url = document.location.href;
-    passSec.domain = extractDomain(document.location.host,tld);
+jQuery(function ($) {
+    chrome.runtime.sendMessage({"name": 'TLD'}, function (tld) {
+        passSec.url = document.location.href;
+        passSec.domain = extractDomain(document.location.host, tld);
 
-    chrome.runtime.sendMessage({name:"getStorage"},function(r){
-      getSecurityStatus(r);
-      processInputs(r);
+        chrome.runtime.sendMessage({name: "getStorage"}, function (r) {
+            getSecurityStatus(r);
+            processInputs(r);
 
-      $('body').on('mouseenter', 'input', function(e) {
-        var o = this;
-        if ( (o.type == "password" || o.type == "search") && !o.classList.contains("passSecNoTooltip")) {
-          passSec.target = this;
-          // Show the qtip
-          $(o).qtip({
-            overwrite: true,
-            suppress: true,
-            content:  {
-              text: getTexts(),
-              button: true
-            },
-            show: {
-              event: e.type,
-              ready: true,
-              solo: true
-            },
-            hide: {
-              fixed: true,
-              hide: 'mouseleave',
-              delay: 1000
-            },
-            position: {
-              at: 'left bottom',
-              my: 'top left',
-              target: 'event',
-              viewport: $(window),
-              adjust: {
-                mouse: false,
-                method: 'flipinvert flipinvert',
-                scroll: false
-              }
-            },
-            style: {
-              tip:false,
-              classes: 'passSecTooltip',
-              widget: true,
-              def: false
-            },
-            events: {
-              render: function(event, api) {
-                passSec.api = api;
-                passSec.tooltip = api.elements.content;
-                processTooltip();
-              }
-            }
-          }, e);
-        }
-      }).on('mouseleave', 'a', function(e){
-        $(this).qtip('destroy');
-      })
+            $('body').on('focus', 'input', function (e) {
+                var o = this;
+                if ((o.type == "password" || o.type == "search") && !o.classList.contains("passSecNoTooltip")) {
+                    passSec.target = this;
+                    // Show the qtip
+                    $(o).qtip({
+                        overwrite: true,
+                        suppress: true,
+                        content: {
+                            text: getTexts()
+                        },
+                        show: {
+                            event: e.type,
+                            ready: true,
+                            solo: true,
+                            delay: 0
+                        },
+                        hide: {
+                            fixed: true,
+                            event: 'unfocus'
+                        },
+                        position: {
+                            at: 'left bottom',
+                            my: 'top left',
+                            viewport: true,
+                            adjust: {
+                                method: 'shift none',
+                                scroll: false
+                            }
+                        },
+                        style: {
+                            tip: false,
+                            classes: 'passSecTooltip',
+                            widget: true,
+                            def: false
+                        },
+                        events: {
+                            render: function (event, api) {
+                                passSec.api = api;
+                                passSec.tooltip = api.elements.content;
+                                processTooltip();
+                            }
+                        }
+                    }, e);
+                }
+            })
+        });
     });
-  });
 });
