edb10af16a1c76aaaf0b2c673660f62c4b1ba189
Johannes <wagener.johannes@googlemail.com>
2017-09-27 17:39:49 +0200

    Optimized detection of the security status

--- js/securitystatus.js
+++ js/securitystatus.js
@@ -3,38 +3,36 @@ passSec.sslCheckResult = "DNS";
 passSec.security = "http";
 
 /**
-*   get security status of website
-*/
-function getSecurityStatus(r){
-  if(r.sslCheckEnabled == "false"){
-    if(passSec.url.startsWith("https")){
-      passSec.security = "https";
+ *   get security status of website
+ */
+function getSecurityStatus(r) {
+    if (passSec.url.startsWith("https")) {
+        passSec.security = "https";
+        if (r.sslCheckEnabled === "true")
+            getSSLStatus();
+    } else {
+        passSec.security = "http";
     }
-    else passSec.security ="http";
-  }
-  else{
-    getSSLStatus();
-  }
-};
+}
 
 /**
-*   TODO: get answer for request to ssllabs api (not ready yet)
-*/
-function getSSLStatus(){
-  chrome.runtime.sendMessage({name: "sslCheck", url: passSec.url},function(r){
-    //console.log(r);
-    var result = JSON.parse(r);
-    //console.log(result.status);
-    var repeatSSLCheck = setInterval(sendRequest(), 2000);
-    passSec.sslCheckResult = result.status;
-    if(passSec.sslCheckResult == "READY"){
-      clearInterval(repeatSSLCheck);
-      //console.log(result.endpoints[0]);
-    }
-  });
-  chrome.runtime.sendMessage({name: "sslcheck", url: passSec.url},function(r){
-    var result = JSON.parse(r);
-    //console.log(result.endpoints[0].statusMessage);
-    passSec.sslCheckResult = result.status;
-  });
+ *   TODO: get answer for request to ssllabs api (not ready yet)
+ */
+function getSSLStatus() {
+    // chrome.runtime.sendMessage({name: "sslCheck", url: passSec.url}, function (r) {
+    //     //console.log(r);
+    //     var result = JSON.parse(r);
+    //     //console.log(result.status);
+    //     var repeatSSLCheck = setInterval(sendRequest(), 2000);
+    //     passSec.sslCheckResult = result.status;
+    //     if (passSec.sslCheckResult == "READY") {
+    //         clearInterval(repeatSSLCheck);
+    //         //console.log(result.endpoints[0]);
+    //     }
+    // });
+    // chrome.runtime.sendMessage({name: "sslcheck", url: passSec.url}, function (r) {
+    //     var result = JSON.parse(r);
+    //     //console.log(result.endpoints[0].statusMessage);
+    //     passSec.sslCheckResult = result.status;
+    // });
 }
